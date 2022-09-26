d4f38643d8bce1adbf99a0fb22f7a147f81fbe95
“Maverick” <cmaverickce@gmail.com>
2022-09-26 15:34:47 -0400

    alias

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,9 @@
 import Counter from "../islands/Counter.tsx";
+import { useMachine } from "@xstate/react";
+import { counterMachine } from "../xstate/example.xstate.ts";
 
 export default function Home() {
+  //  const [state, send] = useMachine(counterMachine);
   return (
     <div class="p-4 mx-auto max-w-screen-md">
       <img
